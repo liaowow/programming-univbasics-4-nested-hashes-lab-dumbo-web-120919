2af6a5465bbd73a1c808733bd545d4b0b0e9daaf
Annie Liao <liaowow@yahoo.com>
2019-11-13 17:02:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -20,7 +20,8 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	programmer_hash[:alan_kay][:known_for]
+	puts programmer_hash
 	
 	programmer_hash = 
  		{
